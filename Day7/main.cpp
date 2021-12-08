0b187342ece0ff61aeed75381ba4594896ca3f8e
Neeraj Singhal <Neerajsinghal93@gmail.com>
2021-12-08 19:56:17 +0100

    Modified Day7 Program WIP

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -4,21 +4,19 @@
  * @date 2021-12-04
  */
 
-#include "lib.hpp"
 #include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <sstream>
-#include <string>
-#include <vector>
-//#define TEST 1
+
+#include "lib.hpp"
+
+#define TEST 1
 #define Case 1
 
 int main() {
   sayHello();
   std::vector<int> inputVector;
-  std::string line;
-  ifstream inFile; 
 
 #if TEST == 0
   std::string filename{""};
@@ -27,31 +25,13 @@ int main() {
 #else
   std::string filename{"../inputs/sonarData.txt"};
 #endif
-
-  inFile.open(filename);
-  if (!inFile) {
-    cout << "Unable to open file";
-    exit(1); // terminate with error
-  }
-
-  while (inFile >> line) {
-    std::stringstream ss(line);
-    for (int i; ss >> i;) {
-      inputVector.emplace_back(i);
-      if (ss.peek() == ',')
-        ss.ignore();
-    }
-  }
-
+  const std::string delimiter{'|'};
+  getFileContentsInVector<int>(inputVector, filename, delimiter);
   cout << "\nInput Vector: ";
   printVector(inputVector);
-  inFile.close();
-
 #if Case == 1
 
-#endif
-
-#if Case == 2
+#elif Case == 2
 
 #endif
   return 0;
